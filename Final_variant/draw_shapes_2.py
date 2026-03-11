2603f42c02dcb0cd62be5ba4d0e4b18503df9b32
Lyazzat1215 <lyazzat.onerkhan06@mail.ru>
2026-03-11 23:04:44 +0500

    Improve code readability and comments in draw_shapes_2.py
    Refactor comments and formatting for clarity.

--- Final_variant/draw_shapes_2.py
+++ Final_variant/draw_shapes_2.py
@@ -7,48 +7,47 @@ import math
 class TurtleDraw(Node):
     def __init__(self):
         super().__init__('turtle_draw')
-        self.pub = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
+        self.pub =self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
         time.sleep(1)  # небольшая пауза перед началом
 
         self.draw_square()
-        self.move_forward(3.0)  # отступ перед следующей фигурой
+        self.move_forward(3.0)  # отступ
         self.draw_circle()
         self.move_forward(3.0)  # ещё один отступ
         self.draw_triangle()
 
     def move(self, linear, angular, duration):
-        msg = Twist()
-        msg.linear.x = linear
-        msg.angular.z = angular
-        start = time.time()
+        msg=Twist()
+        msg.linear.x=linear
+        msg.angular.z=angular
+        start=time.time()
         while time.time() - start < duration:
             self.pub.publish(msg)
             time.sleep(0.05)
-
-    # Функция просто для перемещения вперёд (для отступов)
+            
     def move_forward(self, distance):
-        # скорость 1 м/с, время = distance / скорость
-        duration = distance / 1.0
+        # v=1 m/s, tome=distance/speed
+        duration=distance/1.0
         self.move(1.0, 0.0, duration)
 
     def draw_square(self):
         for _ in range(4):
-            self.move(1.0, 0.0, 2.0)       # вперед
-            self.move(0.0, math.pi/2, 1.0) # поворот 90°
+            self.move(1.0, 0.0, 2.0)       # Forward
+            self.move(0.0, math.pi/2, 1.0) # 90°
 
     def draw_circle(self):
-        self.move(1.0, 1.0, 6.28)  # полный оборот
+        self.move(1.0, 1.0, 6.28)  # 360 angle
 
     def draw_triangle(self):
         for _ in range(3):
             self.move(1.0, 0.0, 2.0)
-            self.move(0.0, math.pi/3, 1.0) # поворот 60°
+            self.move(0.0, math.pi/3, 1.0) # rotation 60°
 
 def main(args=None):
     rclpy.init(args=args)
-    node = TurtleDraw()
+    node=TurtleDraw()
     rclpy.spin(node)
     rclpy.shutdown()
 
-if __name__ == '__main__':
+if __name__=='__main__':
     main()
